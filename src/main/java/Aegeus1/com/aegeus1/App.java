1447bf4ac3dce05eb6d73fbec476612e2cdf5b71
Matthew <matt11matthew@gmail.com>
2016-08-18 23:04:53 -0400

    first

---
+++ src/main/java/Aegeus1/com/aegeus1/App.java
@@ -0,0 +1,13 @@
+package Aegeus1.com.aegeus1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
